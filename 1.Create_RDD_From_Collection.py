2b6df038a16e69887156d0f76aedb09ab04ead4c
vamsispark601 <74048198+vamsispark601@users.noreply.github.com>
2020-11-06 15:36:00 +0530

    Create 1.Create_RDD_From_Collection.py

---
+++ 1.Create_RDD_From_Collection.py
@@ -0,0 +1,8 @@
+import findspark 
+findspark.init()
+from pyspark.sql import SparkSession
+sparkSession = SparkSession.builder.getOrCreate() # Create the sparkSession
+sc = sparkSession.sparkContext # Create the sparkContext
+lst = [11, 22, 33, 44, 55] 
+intRDD = sc.parallelize(lst) # Create RDD from a collection(example: list)
+intRDD.collect()
